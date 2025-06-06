ea78ebf2495d8ebe2ac2fc739b38bc4750b2e10f
Conor Devilly <conor.devilly@eidasolutions.com>
2025-06-06 10:40:24 +0100

    Fix LSP autoconnect

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,33 +1,52 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-        ensure_installed = {
-            "bashls",
-            "dockerls",
-            "html",
-            "java_language_server",
-            "ts_ls",
-            "lua_ls",
-            "pyright",
-            "terraformls",
-        }
+    {
+        "mason-org/mason.nvim",
+        build = ":MasonUpdate",
+        config = function()
+            require("mason").setup()
+        end
     },
-    dependencies = {
-        {
-            "mason-org/mason.nvim",
-            opts = {
-            }
+    {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = {
+            "mason-org/mason.nvim"
         },
-        {
-            "neovim/nvim-lspconfig",
-            lazy = false,
-            config = function()
-                local lspconfig = require("lspconfig")
-            end
-        }
     },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local lspconfig = require("lspconfig")
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
 
-    config = function()
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-    end
+            local on_attach = function(client, bufnr)
+                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+                vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+            end
+
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "bashls",
+                    "dockerls",
+                    "html",
+                    "jdtls",
+                    "ts_ls",
+                    "lua_ls",
+                    "pyright",
+                    "terraformls",
+                },
+                handlers = {
+                    function(server_name)
+                        lspconfig[server_name].setup({
+                            on_attach = on_attach,
+                            capabilities = capabilities
+                        })
+                    end
+                }
+            })
+
+        end
+    }
 }
