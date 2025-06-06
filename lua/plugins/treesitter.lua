1c4841e598240814ff83c3ebd08a0c4583323629
Conor Devilly <conordev32@gmail.com>
2025-06-06 21:33:15 +0100

    Added auto-complete

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,26 @@
 return {
-	'nvim-treesitter/nvim-treesitter',
-	branch = 'main',
-	build = ':TSUpdate',
+	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	build = ":TSUpdate",
 
 	config = function()
-		ensure_installed = { "bash", "c", "dockerfile", "go", "html", "java", "javascript", "json", "lua", "python", "terraform", "vim", "vimdoc" }
+		ensure_installed = {
+			"bash",
+			"c",
+			"dockerfile",
+			"go",
+			"html",
+			"java",
+			"javascript",
+			"json",
+			"lua",
+			"python",
+			"terraform",
+			"vim",
+			"vimdoc",
+		}
+		auto_install = true
 		highlight = { enable = true }
 		indent = { enable = true }
-	end
+	end,
 }
