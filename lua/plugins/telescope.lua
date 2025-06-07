e569ec2b174e53c417d6e0efe620d410a76fb449
Conor Devilly <conordev32@gmail.com>
2025-06-07 20:09:41 +0100

    Switched file explorer from Neotree to Telescope File Browser

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,32 +1,34 @@
 return {
     {
-        'nvim-telescope/telescope.nvim', 
-        tab = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        "nvim-telescope/telescope.nvim",
+        tab = "0.1.8",
+        dependencies = {
+            "nvim-telescope/telescope-ui-select.nvim",
+            "nvim-telescope/telescope-file-browser.nvim",
+            "nvim-lua/plenary.nvim",
+        },
 
         config = function()
-            local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>ff', builtin.find_files)
-            vim.keymap.set('n', '<C-f>', builtin.find_files)
-            vim.keymap.set('n', '<leader>fg', builtin.git_files)
-            vim.keymap.set('n', '<leader>gs', builtin.grep_string)
-            vim.keymap.set('n', '<leader>lg', builtin.live_grep)
-        end
-    },
-    {
-        'nvim-telescope/telescope-ui-select.nvim',
-
-        config = function()
-            require("telescope").setup {
+            local builtin = require("telescope.builtin")
+            require("telescope").setup({
                 extensions = {
                     ["ui-select"] = {
-                        require("telescope.themes").get_dropdown {
-                        }
-
-                    }
-                }
-            }
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                    ["file_browser"] = {
+                        hijack_netrw = true,
+                    },
+                },
+            })
             require("telescope").load_extension("ui-select")
-        end
+            require("telescope").load_extension("file_browser")
+
+            vim.keymap.set("n", "<leader>ff", builtin.find_files)
+            vim.keymap.set("n", "<C-f>", builtin.find_files)
+            vim.keymap.set("n", "<leader>fg", builtin.git_files)
+            vim.keymap.set("n", "<leader>gs", builtin.grep_string)
+            vim.keymap.set("n", "<leader>lg", builtin.live_grep)
+            vim.keymap.set("n", "<leader>fs", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+        end,
     },
 }
