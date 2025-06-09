a01603f8589aa5ff156bb6266dc938f0dcfcd832
Conor Devilly <conor.devilly@eidasolutions.com>
2025-06-09 15:10:04 +0100

    Fixed JDTLS attaching to everything

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -1,77 +1,62 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"nvim-neotest/nvim-nio",
-		"jbyuki/one-small-step-for-vimkind",
-		"mfussenegger/nvim-dap-python",
-		"mfussenegger/nvim-jdtls",
-	},
-	lazy = false,
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+        "nvim-neotest/nvim-nio",
+        "jbyuki/one-small-step-for-vimkind",
+        "mfussenegger/nvim-dap-python",
+        "mfussenegger/nvim-jdtls",
+    },
+    lazy = false,
 
-	config = function()
-		local dap, dapui = require("dap"), require("dapui")
-		dapui.setup()
+    config = function()
+        local dap, dapui = require("dap"), require("dapui")
+        dapui.setup()
 
-		-- DAP UI Setup
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+        -- DAP UI Setup
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
 
-		-- Keybinds
-		vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {})
-		vim.keymap.set("n", "<leader>do", dap.step_over, {})
-		vim.keymap.set("n", "<leader>dO", dap.step_out, {})
-		vim.keymap.set("n", "<leader>di", dap.step_into, {})
-		vim.keymap.set("n", "<leader>dc", dap.continue, {})
+        -- Keybinds
+        vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {})
+        vim.keymap.set("n", "<leader>do", dap.step_over, {})
+        vim.keymap.set("n", "<leader>dO", dap.step_out, {})
+        vim.keymap.set("n", "<leader>di", dap.step_into, {})
+        vim.keymap.set("n", "<leader>dc", dap.continue, {})
 
-		-- DAP configs
-		-- Neovim Lua
-		dap.configurations.lua = {
-			{
-				type = "nlua",
-				request = "attach",
-				name = "Attach to running Neovim instance",
-			},
-		}
-		dap.adapters.nlua = function(callback, config)
-			callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
-		end
+        -- DAP configs
+        -- Neovim Lua
+        dap.configurations.lua = {
+            {
+                type = "nlua",
+                request = "attach",
+                name = "Attach to running Neovim instance",
+            },
+        }
+        dap.adapters.nlua = function(callback, config)
+            callback({ type = "server", host = config.host or "127.0.0.1", port = config.port or 8086 })
+        end
 
-		-- Python
-		require("dap-python").setup("python3")
+        -- Python
+        require("dap-python").setup("python3")
 
-		-- Java
-		local java_config = {
-			cmd = { vim.fn.stdpath("data") .. "/mason/bin/jdtls" },
-			root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
-			init_options = {
-				bundles = {
-					vim.fn.stdpath("data") .. "/mason/share/java-debug-adapter/com.microsoft.java.debug.plugin.jar",
-				},
-			},
-
-			on_attach = function(client, bufnr)
-				require("jdtls").setup_dap({ hotcodereplace = "auto" })
-				require("jdtls.dap").setup_dap_main_class_configs()
-			end,
-		}
-		require("jdtls").start_or_attach(java_config)
-		dap.configurations.java = {
-			{
-				javaExec = "java",
-				request = "launch",
-				type = "java",
-			},
-		}
-	end,
+        -- Java configured in lsp.lua
+        dap.configurations.java = {
+            {
+                javaExec = "java",
+                request = "launch",
+                type = "java",
+            },
+        }
+    end,
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,89 +1,118 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		dependencies = {
-			"mason-org/mason.nvim",
-			"mason-org/mason-lspconfig.nvim",
-			"hrsh7th/nvim-cmp",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-cmdline",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-		},
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        dependencies = {
+            "mason-org/mason.nvim",
+            "mason-org/mason-lspconfig.nvim",
+            "hrsh7th/nvim-cmp",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "mfussenegger/nvim-jdtls",
+        },
 
-		config = function()
-			local cmp = require("cmp")
-			local cmp_lsp = require("cmp_nvim_lsp")
-			local lspconfig = require("lspconfig")
-			local capabilities = vim.tbl_deep_extend(
-				"force",
-				{},
-				vim.lsp.protocol.make_client_capabilities(),
-				cmp_lsp.default_capabilities()
-			)
+        config = function()
+            local cmp = require("cmp")
+            local cmp_lsp = require("cmp_nvim_lsp")
+            local lspconfig = require("lspconfig")
+            local capabilities = vim.tbl_deep_extend(
+                "force",
+                {},
+                vim.lsp.protocol.make_client_capabilities(),
+                cmp_lsp.default_capabilities()
+            )
 
-			-- Setup completions
-			local cmp_select = { behavior = cmp.SelectBehavior.Select }
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-						vim.snippet.expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-					["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
+            -- Setup completions
+            local cmp_select = { behavior = cmp.SelectBehavior.Select }
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                        vim.snippet.expand(args.body)
+                    end,
+                },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+                    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                }, {
+                    { name = "buffer" },
+                }),
+            })
 
-			-- Setup LSPs
-			require("mason").setup()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"bashls",
-					"dockerls",
-					"html",
-					"jdtls",
-					"ts_ls",
-					"lua_ls",
-					"pyright",
-					"terraformls",
-				},
-				handlers = {
-					function(server_name)
-						lspconfig[server_name].setup({
-							capabilities = capabilities,
-						})
-					end,
-				},
-			})
+            -- Setup LSPs
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "bashls",
+                    "dockerls",
+                    "html",
+                    "jdtls",
+                    "ts_ls",
+                    "lua_ls",
+                    "pyright",
+                    "terraformls",
+                },
+                handlers = {
+                    function(server_name)
+                        lspconfig[server_name].setup({
+                            capabilities = capabilities,
+                        })
+                    end,
+                },
+            })
 
-			-- Enable diagnostics
-			vim.diagnostic.config({ virtual_text = true })
-			vim.diagnostic.enable(true)
+            -- Enable diagnostics
+            vim.diagnostic.config({ virtual_text = true })
+            vim.diagnostic.enable(true)
 
-			-- Setup keybinds
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+            -- Setup keybinds
+            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
+    {
+        "mfussenegger/nvim-jdtls",
+        ft = { "java" },
+        -- TODO: Depenencies
+
+        config = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "java",
+                callback = function()
+                    local data_path = vim.fn.stdpath("data")
+                    local java_config = {
+                        cmd = { data_path .. "/mason/bin/jdtls" },
+                        root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew" }),
+                        init_options = {
+                            bundles = {
+                                data_path .. "/mason/share/java-debug-adapter/com.microsoft.java.debug.plugin.jar",
+                            },
+                        },
+                        on_attach = function(client, bufnr)
+                            require("jdtls").setup_dap({ hotcodereplace = "auto" })
+                            require("jdtls.dap").setup_dap_main_class_configs()
+                        end,
+                    }
+                    require("jdtls").start_or_attach(java_config)
+                end,
+            })
+        end,
+    },
 }
